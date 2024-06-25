b283b02053cb50bab5d2be5a6c8eb017b6dea552
D Sandhu <duvendersandhu@gmail.com>
2024-06-25 17:53:23 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors';
+import 'dotenv/config'
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const todoSchema = new mongoose.Schema({
+  text: String,
+  completed: Boolean,
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+app.get('/todos', async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post('/todos', async (req, res) => {
+  const newTodo = new Todo({
+    text: req.body.text,
+    completed: false,
+  });
+  await newTodo.save();
+  res.json(newTodo);
+});
+
+app.put('/todos/:id', async (req, res) => {
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedTodo);
+});
+
+app.delete('/todos/:id', async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Todo deleted' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
